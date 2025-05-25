dd9cb01457da174e2f64bfb6fcf252c25a62eb5e
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2025-05-25 14:26:37 +0500

    plugin: workmanager setup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import workmanager
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,13 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      WorkmanagerPlugin.setPluginRegistrantCallback { registry in
+          GeneratedPluginRegistrant.register(with: registry)
+      }
+      
+      if #available(iOS 13.0, *) {
+        UNUserNotificationCenter.current().delegate = self
+      }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
